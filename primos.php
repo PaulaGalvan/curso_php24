fcd8c7e379087d9130dddb86c613c1ef460c3492
PaulaGalvan <156858574+PaulaGalvan@users.noreply.github.com>
2024-04-16 20:54:45 -0300

    Update primos.php

--- primos.php
+++ primos.php
@@ -7,3 +7,20 @@
     Ex.: 7 => 7/1 e 7/7.
     Dica: Utilizar o operador de módulo % para pegar o resto da divisão.
 */
+
+for($i = 1; $i <= 12; $i++)
+{
+    $divisores = 0;
+     
+    for($j = $i; $j >= 1; $j--)
+    {
+        if (($i % $j) == 0) {
+            $divisores++;
+        }
+    }
+     
+    if ($divisores == 2)
+    {
+        echo $i . ', ';
+    }
+}
