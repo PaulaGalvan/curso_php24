285258dace3b051aa95d202b49ed468cb44fd502
PaulaGalvan <156858574+PaulaGalvan@users.noreply.github.com>
2024-04-16 21:55:35 -0300

    Update and rename perfeitos to perfeitos.php

---
+++ perfeitos.php
@@ -0,0 +1,37 @@
+<?php
+
+// calcular os 4 primeiros numeros perfeitos
+// um num perfeito é quando a soma dos divisores é igual 
+// ao numero;
+$arrayPerfeitos=[];
+
+
+function isPerfeito($num){
+    $arrayDiv=[];
+    // $num = 6;
+    
+    for ($div = 1; $div < $num; $div++){
+        
+        if ($num % $div == 0){
+            $arrayDiv[]=$div;
+        }
+    }
+    
+    $totalDiv= array_sum($arrayDiv);
+    
+    if ($totalDiv == $num){
+        return true;
+    }
+}
+
+$num=2;
+
+while (count($arrayPerfeitos) < 4){
+    if(isPerfeito($num)){
+        $arrayPerfeitos[]=$num;
+    }
+    
+    $num++;
+}
+
+echo implode(" , ", $arrayPerfeitos);
